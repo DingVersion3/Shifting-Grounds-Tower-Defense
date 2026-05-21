f424065f61d56e439c2952aa637ad46800db9525
DingVersion3 <jakewilk10@gmail.com>
2026-05-21 09:28:47 -0700

    fps set to 60

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    dt = 0.0
     while True:
         log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
         pygame.display.flip()
+        dt = fps.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
