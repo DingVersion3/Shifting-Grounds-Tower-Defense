cbafd77913c558fcb4b2f7f8dc8b13d444be3605
DingVersion3 <jakewilk10@gmail.com>
2026-05-20 20:30:00 -0700

    initial pygame setup

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
-    print('Hello Ding!')
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
