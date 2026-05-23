69ef1d0b96f0cd804d4a46b0c505df56b72cca24
DingVersion3 <jakewilk10@gmail.com>
2026-05-23 09:52:48 -0700

    money checks for tower clicks

--- main.py
+++ main.py
@@ -52,11 +52,11 @@ def main():
                             tower_y = event.pos[1] // CELL_SIZE
                             cell = game_map.grid[tower_y][tower_x]
                             if cell.cell_type not in ("Road", "Start", "End"):
-                                if selected_tower == "basic":
+                                if selected_tower == "basic" and player.money >= 100:
                                     Tower(tower_x, tower_y)
-                                elif selected_tower == "jt":
+                                elif selected_tower == "jt" and player.money >= 250:
                                     JTTower(tower_x, tower_y)
-                                elif selected_tower == "laser":
+                                elif selected_tower == "laser" and player.money >= 500:
                                     LaserTower(tower_x, tower_y)
         updateable.update(dt, enemies, player)
         if player.health <= 0:
