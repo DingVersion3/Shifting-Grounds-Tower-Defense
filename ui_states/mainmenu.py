f9c52718d47f0c1ee6ad0c8edb95ada68188cbc0
DingVersion3 <jakewilk10@gmail.com>
2026-06-19 11:52:33 -0700

    multiplayer menu basics

--- ui_states/mainmenu.py
+++ ui_states/mainmenu.py
@@ -93,7 +93,7 @@ class SettingsMenu():
 
         try:
             self.font = pygame.Font('assets/Fonts/Kenney_Future_Narrow.ttf', 70)
-            self.small_font = pygame.Font('assets/Fonts/Kenney_Future_Narrow.ttf', 36)
+            self.small_font = pygame.Font('assets/Fonts/Kenney_Future_Narrow.ttf', 30)
             self.label_font = pygame.Font('assets/Fonts/Kenney_Future_Narrow.ttf', 30)
         except:
             self.font = pygame.font.Font(None, 70)
@@ -195,6 +195,102 @@ class SettingsMenu():
             pygame.display.flip()
 
 
+class MultiplayerMenu():
+    def __init__(self, screen):
+        self.screen = screen
+        self.clock = pygame.time.Clock()
+        self.running = True
+        #string that stores the ip address for multiplayer sessions
+        self.ip_input = "420.0.6.7"
+        self.input_active = False
+
+        #fonts fall back checks
+        try:
+            self.font = pygame.Font('assets/Fonts/Kenney_Future_Narrow.ttf', 36)
+            self.label_font = pygame.Font('assets/Fonts/Kenney_Future_Narrow.ttf', 30)
+        except:
+            self.font = pygame.Font(None, 36)
+            self.label_font = pygame.Font(None, 36)
+
+        theme_blue = (40, 60, 150)
+        theme_hover = (60, 90, 220)
+        theme_border = (200, 200, 255)
+        btn_width, btn_height = 250, 60
+        center_x = SCREEN_WIDTH // 2 - (btn_width // 2)
+
+        self.connect_btn = MenuButton(
+            center_x, SCREEN_HEIGHT // 2 + 10,
+            btn_width, btn_height, "Connect",
+            self.label_font, theme_blue, theme_hover, theme_border,
+            border_thickness=3, border_radius=12
+        )
+        self.back_btn = MenuButton(
+            center_x, SCREEN_HEIGHT // 2 + 90,
+            btn_width, btn_height, "Back",
+            self.label_font, theme_blue, theme_hover, theme_border,
+            border_thickness=3, border_radius=12
+        )
+        self.input_rect = pygame.Rect(SCREEN_WIDTH // 2 - 150, SCREEN_HEIGHT // 2 - 80, 300, 50)
+
+    def display(self, network_client=None):
+        #pass network to the internet while player is interacting with the menu
+        while self.running:
+            dt = self.clock.tick(60) / 1000.0
+            self.screen.fill((20, 20, 20))
+
+            #let network process background socket packets
+            if network_client:
+                network_client.update()
+                #check if the network connected successfully
+                if network_client.is_connected:
+                    self.running = False
+                    return "START_MULTIPLAYER_GAME"
+                
+            title = self.font.render("Multiplayer", True, (255, 255, 255))
+            title_rect = title.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 180))
+            self.screen.blit(title, title_rect)
+
+            box_border_color = (40, 130, 80) if self.input_active else (100, 100, 120)
+            pygame.draw.rect(self.screen, (10, 10, 10), self.input_rect, border_radius=8) # filled dark background
+            pygame.draw.rect(self.screen, box_border_color, self.input_rect, width=2, border_radius=8)
+
+            ip_text_surf = self.label_font.render(self.ip_input, True, (255, 255, 255))
+            self.screen.blit(ip_text_surf, (self.input_rect.x + 15, self.input_rect.y + 10))
+
+            mouse_pos = pygame.mouse.get_pos()
+            self.connect_btn.hover(mouse_pos)
+            self.back_btn.hover(mouse_pos)
+
+            self.connect_btn.draw(self.screen)
+            self.back_btn.draw(self.screen)
+                
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return "QUIT"
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    self.input_active = self.input_rect.collidepoint(event.pos)
+                if self.back_btn.clicked(event):
+                    self.running = False
+                    return "MENU"
+                if self.connect_btn.clicked(event):
+                    if network_client:
+                        network_client.start_connection(self.ip_input)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.running = False
+                        return "MENU"
+                    if self.input_active:
+                        if event.key == pygame.K_BACKSPACE:
+                            self.ip_input = self.ip_input[:-1]
+                        elif event.key in (pygame.K_RETURN, pygame.K_KP_ENTER):
+                            self.input_active = False
+                        else:
+                            if event.unicode in "0123456789." and len(self.ip_input) < 15: #max length for ip address, acccepts numbers and decimals only
+                                self.ip_input += event.unicode
+            pygame.display.flip()
+
+
 class MainMenu():
     def __init__(self, screen):
         self.screen = screen
@@ -240,10 +336,10 @@ class MainMenu():
         # Explicit font check fallback system prevents text object crashing
         try:
             self.font = pygame.Font('assets/Fonts/Kenney_Future_Narrow.ttf', 70)
-            self.small_font = pygame.Font('assets/Fonts/Kenney_Future_Narrow.ttf', 36)
+            self.small_font = pygame.Font('assets/Fonts/Kenney_Future_Narrow.ttf', 30)
         except:
             self.font = pygame.font.Font(None, 70)
-            self.small_font = pygame.font.Font(None, 36)
+            self.small_font = pygame.font.Font(None, 30)
             
         self.running = True
         self.clock = pygame.time.Clock()
@@ -251,19 +347,21 @@ class MainMenu():
         btn_width, btn_height = 250, 60
         center_x = SCREEN_WIDTH // 2 - (btn_width // 2)
         start_y = SCREEN_HEIGHT // 2
-        quit_y = SCREEN_HEIGHT // 2 + 80
+        multiplayer_y = SCREEN_HEIGHT  // 2 + 80
         settings_y = SCREEN_HEIGHT // 2 + 160
+        quit_y = SCREEN_HEIGHT // 2 + 240
         
         theme_blue = (40, 60, 150)
         theme_hover = (60, 90, 220)
         theme_border = (200, 200, 255)
         
         self.start_btn = MenuButton(center_x, start_y, btn_width, btn_height, "Start Game", self.small_font, theme_blue, theme_hover, theme_border, border_thickness=3, border_radius=12)
-        self.quit_btn = MenuButton(center_x, quit_y, btn_width, btn_height, "Quit", self.small_font, (120, 30, 30), (180, 40, 40), theme_border, border_thickness=3, border_radius=12)
+        self.multiplayer_btn = MenuButton(center_x, multiplayer_y, btn_width, btn_height, "Multiplayer", self.small_font, theme_blue, theme_hover, theme_border, border_thickness=3, border_radius=12)
         self.settings_btn = MenuButton(center_x, settings_y, btn_width, btn_height, "Settings", self.small_font, (128, 128, 128), (180, 180, 180), theme_border, border_thickness=3, border_radius=12)
+        self.quit_btn = MenuButton(center_x, quit_y, btn_width, btn_height, "Quit", self.small_font, (120, 30, 30), (180, 40, 40), theme_border, border_thickness=3, border_radius=12)
 
     def _spawn_fake_towers(self, Basic, JT, Laser, Sniper):
-        """Finds valid spots on the map grid and sprinkles a few random towers."""
+        #Finds valid spots on the map grid and sprinkles a few random towers.
         import random
         tower_types = [Basic, JT, Laser, Sniper]
         
@@ -276,7 +374,7 @@ class MainMenu():
         
         # Shuffle coordinates and pick a fixed amount of spots (e.g., 6 random spots)
         random.shuffle(valid_coords)
-        spots_to_place = min(6, len(valid_coords)) 
+        spots_to_place = min(10, len(valid_coords)) 
         
         for i in range(spots_to_place):
             tx, ty = valid_coords[i]
@@ -285,7 +383,7 @@ class MainMenu():
             chosen_tower_class(tx, ty)
 
     def _update_and_draw_bg(self, dt):
-        """Helper to advance and render the fake background scene frame step."""
+        #Helper to advance and render the fake background scene frame step.
         # Update custom background logic systems
         self.bg_wave_manager.update(dt, self.bg_updateable, self.bg_drawable, self.bg_enemies)
         
@@ -333,12 +431,14 @@ class MainMenu():
             
             mouse_pos = pygame.mouse.get_pos()
             self.start_btn.hover(mouse_pos)
-            self.quit_btn.hover(mouse_pos)
+            self.multiplayer_btn.hover(mouse_pos)
             self.settings_btn.hover(mouse_pos)
+            self.quit_btn.hover(mouse_pos)
 
             self.start_btn.draw(self.screen)
-            self.quit_btn.draw(self.screen)
+            self.multiplayer_btn.draw(self.screen)
             self.settings_btn.draw(self.screen)
+            self.quit_btn.draw(self.screen)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -358,6 +458,16 @@ class MainMenu():
                     self.screen = pygame.display.get_surface()
                     if result == "QUIT":
                         return "QUIT"
+                    
+                if self.multiplayer_btn.clicked(event):
+                    multi_screen = MultiplayerMenu(self.screen)
+                    result = multi_screen.display(network_client=None)
+                    self.screen = pygame.display.get_surface()
+                    if result == "QUIT":
+                        return "QUIT"
+                    elif result == "START_MULTIPLAYER_GAME":
+                        self.running = False
+                        return "START_MULTIPLAYER"
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_s:
                         self.running = False
